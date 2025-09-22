c7a9844291e109a86c693073e8fd2bfa876b15a3
Makiko <trimakichan@gmail.com>
2025-09-22 14:51:46 -0700

    add min function

--- function_d.py
+++ function_d.py
@@ -4,6 +4,11 @@ def max_value(numbers):
     """
     return max(numbers)
 
+def min_value(numbers):
+    return min(numbers)
+
 
 if __name__ == "__main__":
     print(max_value([1, 12, 2, 42, 8, 3]))
+    print(min_value([1, 12, 2, 42, 8, 3]))
+
